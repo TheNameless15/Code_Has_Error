f8b008f30eddffc1916f25e4ef809ee5ad6f9e0d
TheNameless15 <135017490+TheNameless15@users.noreply.github.com>
2023-12-04 09:51:39 +0700

    After optimize

--- src/main/java/vn/edu/bkacad/Program.java
+++ src/main/java/vn/edu/bkacad/Program.java
@@ -1,51 +1,51 @@
-package vn.edu.bkacad;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Program {
+    private static final String URL = "jdbc:mysql://localhost:3306/programming_code_has_error";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
     public static void main(String[] args) {
-        System.out.println("** Program search list of students whose name contain value input **");
+        System.out.println("** Program search list of students whose name contains the input value **");
 
         Scanner sc = new Scanner(System.in);
+        String inputValue;
 
-        System.out.println("Enter value:");
-        String a = sc.nextLine();
-        while (a.length() == 0 || a.equals("")) {
-            System.out.println("Please enter student name");
+        do {
             System.out.println("Enter value:");
-            a = sc.nextLine();
-        }
+            inputValue = sc.nextLine().trim();
+        } while (inputValue.isEmpty());
 
-        String URL = "jdbc:mysql://localhost:3306/programming_code_has_error";
-        String USERNAME = "root";
-        String PASSWORD = "";
+        String query = "SELECT * FROM student WHERE full_name LIKE ?";
 
-        try {
-            String str = "SELECT * FROM student WHERE full_name LIKE '%" + a + "%'";
+        try (
+                Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                PreparedStatement st = con.prepareStatement(query);
+        ) {
+            st.setString(1, "%" + inputValue + "%");
 
-            Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            Statement st = con.createStatement();
+            try (ResultSet rs = st.executeQuery()) {
+                System.out.println("** List of students **");
 
-            ResultSet rs = st.executeQuery(str);
+                StringBuilder output = new StringBuilder();
+                while (rs.next()) {
+                    String fullname = rs.getString("full_name");
+                    String studentcode = rs.getString("student_code");
+                    String address = rs.getString("address");
+                    Date birthday = rs.getDate("birthday");
 
-            System.out.println("** List of students **");
-            while (rs.next()) {
-                String fullname = rs.getString("full_name");
-                String studentcode = rs.getString("student_code");
-                String address = rs.getString("address");
-                Date birthday = rs.getDate("birthday");
+                    output.append("Full name: ").append(fullname).append("\n");
+                    output.append("Student code: ").append(studentcode).append("\n");
+                    output.append("Address: ").append(address).append("\n");
+                    output.append("Birthday: ").append(birthday).append("\n\n");
+                }
 
-                System.out.println("Full name: " + fullname);
-                System.out.println("Student code: " + studentcode);
-                System.out.println("Address: " + address);
-                System.out.println("Birthday: " + birthday);
-
-                System.out.println("");
+                System.out.println(output.toString().trim());
             }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
